9e4406e98855ea0fbef3c44167325f518c5bb538
ACwolf55 <66705643+ACwolf55@users.noreply.github.com>
2021-12-19 23:03:24 -0700

    sql functions

--- jobs/app.py
+++ jobs/app.py
@@ -1,8 +1,20 @@
+from sqlite3.dbapi2 import connect
 from flask import Flask, render_template, g
 import sqlite3
 
 app = Flask(__name__)
 PATH = "db/jobs.sqlite"
+
+def open_connection():
+    connection = getattr(g,'_connection', None)
+    if connection == None:
+        connection = g._connection = sqlite3.connect(PATH)
+    connection.row_factory = sqlite3.Row
+    return connection
+
+def execute_sql(sql,values=(),commit=False,single=False):
+    connection = open_connection()
+
 
 @app.route('/')
 @app.route('/jobs')
