6429c836826f443038870dbeaed57aa5ade80367
ACwolf55 <66705643+ACwolf55@users.noreply.github.com>
2021-12-19 23:34:46 -0700

    execute sql def

--- jobs/app.py
+++ jobs/app.py
@@ -14,6 +14,18 @@ def open_connection():
 
 def execute_sql(sql,values=(),commit=False,single=False):
     connection = open_connection()
+    cursor = connection.execute(sql,values)
+    if commit == True:
+        results = connection.commit()
+    else:
+        results = cursor.fetchone() if single else cursor.fetchall()
+    
+    cursor.close()
+    return results
+
+def close_connection(exception):
+    connection = getattr(g,'_connection', None)
+
 
 
 @app.route('/')
